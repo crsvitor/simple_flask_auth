da2b2aae11097db95d96e4145a4793ee95684a47
crsvitor <dev.castrors@gmail.com>
2024-07-07 19:21:43 -0300

    feat: add flask login

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_login import LoginManager
 from database import db
 
 app = Flask(__name__)
+login_manager = LoginManager()
+
 app.config["SECRET_KEY"] = "my_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db.init_app(app)
+login_manager.init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from database import db
+from flask_login import UserMixin
 
-class User(db.model):
+class User(db.model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
