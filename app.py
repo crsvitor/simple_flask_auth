8743e09bc337699c0461d6a24c3a27b14efa8330
crsvitor <dev.castrors@gmail.com>
2024-07-07 19:18:33 -0300

    feat: add app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from database import db
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "my_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db.init_app(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
